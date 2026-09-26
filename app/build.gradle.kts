c284057e02f3365388701a01d30dd32be05dd0c3
lzh <lzh1064433607@163.com>
2026-09-26 21:14:21 +0800

    最低兼容安卓8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lzh.smartbill"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
